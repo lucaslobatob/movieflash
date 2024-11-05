6656e76435236da5adf68fa11dbbb591620c6344
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2024-11-05 17:13:30 -0300

    Add API in page Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+import api from "../../../services/api";
+
 function Home() {
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+
+        async function loadingFilmes() {
+            const response = await api.get("movie/now_playing", {
+                params: {
+                    api_key: "6648f19b8f09e1fc8d551f5926a5fcb3",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+
+            console.log(response.data.results);
+        }
+
+        loadingFilmes();
+    }, [])
     return (
         <div>
             <h1>Bem-vindo ao nossa Home</h1>

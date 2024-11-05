4106f80c031fc5252086529af3524425b008ef79
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2024-11-04 23:28:03 -0300

    Add header component  in routes

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home.js";
 import Filme from "./pages/Filme/Filme.js";
+import Header from "./components/Header/Header.js";
 
 function RoutesApp() {
     return (
         <BrowserRouter>
+        <Header />
         <Routes>
             <Route path='/' element={ <Home />} />
             <Route path='/filme/:id' element={ <Filme />} />

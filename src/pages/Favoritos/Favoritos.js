4b8f609d26270428380c696b5c8deae571edab89
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2024-11-26 16:34:17 -0300

    Add function Delete Movie

--- src/pages/Favoritos/Favoritos.js
+++ src/pages/Favoritos/Favoritos.js
@@ -9,11 +9,22 @@ function Favoritos() {
 
         const minhaLista = localStorage.getItem("@movieFlash");
         setFilmes(JSON.parse(minhaLista) || []);
-    }, [])
+    }, []);
+
+    function excluirFilme(id) {
+        let filtroFilmes = filmes.filter((item) => {
+            return (item.id !== id);
+        });
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem("@movieFlash", JSON.stringify(filtroFilmes));
+    };
 
     return (
         <div className='meus-filmes'>
             <h1>Meus favoritos</h1>
+
+            {filmes.length === 0 && <span>Você não possui nenhum filme salvo.</span>}
 
             <ul>
                 {filmes.map((item) => {
@@ -23,7 +34,7 @@ function Favoritos() {
 
                             <div>
                                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
                             </div>
                         </li>
                     )
